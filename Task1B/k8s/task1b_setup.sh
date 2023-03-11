1e4db655bd8401f0044ee3b8b7ad57ad36ee4cf7
denny-wongso <denny.wongso@gmail.com>
2023-03-12 01:12:25 +0800

    added sh scripts for task1b

--- Task1B/k8s/task1b_setup.sh
+++ Task1B/k8s/task1b_setup.sh
@@ -1,1 +1,12 @@
 # Place your commands here
+kind create cluster --name kind-1 --config kind/cluster-config.yaml
+
+kubectl apply -f manifests/k8s/backend-deployment.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+kubectl apply -f manifests/k8s/backend-service.yaml
+
+kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission
+
+kubectl apply -f manifests/k8s/backend-ingress.yaml
